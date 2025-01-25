6894dbdcb16f08212f4bacc37301b4098ada059e
bharat sharma <bharat1.sharma@outlook.com>
2025-01-25 23:02:55 +0530

    yml add

--- s3.tf
+++ s3.tf
@@ -38,3 +38,44 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+resource "aws_s3_bucket" "my_bucket1" {
+  bucket = "test-hclnew2"
+  acl    = "private" 
+
+  tags = {
+    Environment = "Dev"
+    Team        = "DevOps"
+  }
+}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket1.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.my_bucket1.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256" # Default encryption algorithm
+    }
+  }
+}
+
+# Block public access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_bucket1.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
